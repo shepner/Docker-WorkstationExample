09357dbdebe323375b27d70eace153d0d81974bc
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-05-02 09:35:37 -0500

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#export DISPLAY=:1
+Xvfb :1 -screen 0 1280x740x16 &
+sleep 5
+x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
+
+#launch apps
+openbox-session &  # window manager
+
+#$HOME/install_calibre.sh
+#$HOME/calibre/calibre &
+
+$HOME/noVNC/utils/launch.sh --vnc localhost:5900
